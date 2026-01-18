15bd01aa584ad78d6095a9ece0e5124722cf04de
Franciszek Przybyłowski <Franekprzybylowski@gmail.com>
2026-01-18 15:55:19 +0100

    Update AppDBContext.cs

--- backend/AppDbContext.cs
+++ backend/AppDbContext.cs
@@ -8,5 +8,6 @@ namespace Backend
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Report> Reports { get; set; }
+        public DbSet<Meal> Meals { get; set; }
     }
 }
